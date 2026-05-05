bb6630df832bc80b3b709309d05c1b6437c05f43
Manya Shukla <work.shuklamanya@gmail.com>
2026-05-05 18:54:23 +0530

    feat: update features and improve system performance

--- frontend-web/components/layout/ResponsiveSidebarShell.tsx
+++ frontend-web/components/layout/ResponsiveSidebarShell.tsx
@@ -78,7 +78,7 @@ export function ResponsiveSidebarShell({ children, sidebar, mobileTopBarTitle }:
         </div>
       </aside>
 
-      <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-x-hidden pt-14 md:ml-64 md:pt-0">
+      <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-x-hidden pt-14 md:pl-64 md:pt-0">
         <div className="ev-shell-body flex min-h-0 min-w-0 flex-1 flex-col">{children}</div>
       </div>
     </div>
